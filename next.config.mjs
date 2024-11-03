939b95d15104b674e1263825133eb6bd8b16cfdd
David-Shahmatov006 <david.shahmatov.work@gmail.com>
2024-11-03 19:09:23 +0200

    fixes with configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    assetPrefix: '/Car-dealer-app/',
+    basePath: '/Car-dealer-app',
 };
 
 export default nextConfig;
