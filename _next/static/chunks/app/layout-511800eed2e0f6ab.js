ac8d7730a2bca8e5611294944dcbe47b5c9839ec
David-Shahmatov006 <david.shahmatov.work@gmail.com>
2024-11-03 18:40:40 +0200

    Updates

---
+++ _next/static/chunks/app/layout-511800eed2e0f6ab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{686:function(n,e,u){Promise.resolve().then(u.t.bind(u,3177,23))},3177:function(){}},function(n){n.O(0,[120,971,596,744],function(){return n(n.s=686)}),_N_E=n.O()}]);
